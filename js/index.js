466e230dd63df2621c88a9a7f5f0a25b5b8e234e
nasciontem <lucas.tahansab@yahoo.com>
2021-04-21 16:56:37 -0300

    Adjusted slideshow view

--- js/index.js
+++ js/index.js
@@ -10,12 +10,21 @@ window.addEventListener('load', () => {
   document.getElementById('50minutes').addEventListener('click', event => handleTimerSelection(event))
   document.getElementById('execute-button').addEventListener('click', () => handleTimerExecution())
   document.getElementById('reload-button').addEventListener('click', () => handleTimerReload())
+  document.getElementById('left-last').addEventListener('click', () => goToLast())
+  document.getElementById('right-second').addEventListener('click', () => goToRight())
+  document.getElementById('left-home').addEventListener('click', () => goToLeft())
+  document.getElementById('right-third').addEventListener('click', () => goToRight())
+  document.getElementById('left-second').addEventListener('click', () => goToLeft())
+  document.getElementById('right-fourth').addEventListener('click', () => goToRight())
+  document.getElementById('left-third').addEventListener('click', () => goToLeft())
+  document.getElementById('right-last').addEventListener('click', () => goToRight())
+  document.getElementById('left-fourth').addEventListener('click', () => goToLeft())
+  document.getElementById('right-home').addEventListener('click', () => goToHome())
   startTimer()
 })
 
-let setIntervalId = null
 function startTimer() {
-  setIntervalId = setInterval(() => {
+  setInterval(() => {
     if (timer.isPlaying) {
       timer.decreaseTimer()
       changeTimerValueOnScreen()
@@ -68,4 +77,26 @@ function changeExecuteImage() {
 
 function playAlarm() {
   document.getElementById('alarm').play()
+}
+
+function goToRight() {
+  const slideShow = document.getElementById('slide-show')
+  const slideSize = slideShow.firstElementChild.clientWidth + (2 * slideShow.firstElementChild.offsetLeft)
+  slideShow.scrollLeft += slideSize
+}
+
+function goToLeft() {
+  const slideShow = document.getElementById('slide-show')
+  const slideSize = slideShow.firstElementChild.clientWidth + (2 * slideShow.firstElementChild.offsetLeft)
+  slideShow.scrollLeft -= slideSize
+}
+
+function goToHome() {
+  const slideShow = document.getElementById('slide-show')
+  slideShow.scrollLeft -= slideShow.scrollWidth
+}
+
+function goToLast() {
+  const slideShow = document.getElementById('slide-show')
+  slideShow.scrollLeft += slideShow.scrollWidth
 }
