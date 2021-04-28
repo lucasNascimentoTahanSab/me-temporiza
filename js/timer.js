e790af808665c8f6c68cb9bc29d2b5855df8f6ab
nasciontem <lucas.tahansab@yahoo.com>
2021-04-28 19:00:14 -0300

    Created package.json and used jQuery for handling custom time selection

--- js/timer.js
+++ js/timer.js
@@ -1,100 +1,19 @@
 export default class Timer {
-  _initialHours
-  _initialMinutes
-  _initialSeconds
-  _currentHours
-  _currentMinutes
-  _currentSeconds
-  _isPlaying
+  initialHours
+  initialMinutes
+  initialSeconds
+  currentHours
+  currentMinutes
+  currentSeconds
+  isPlaying
 
   constructor() {
-    this._initialHours = '00'
-    this._initialMinutes = '05'
-    this._initialSeconds = '00'
-    this._currentHours = parseInt(this._initialHours)
-    this._currentMinutes = parseInt(this._initialMinutes)
-    this._currentSeconds = parseInt(this._initialSeconds)
-    this._isPlaying = false
-  }
-
-  get isPlaying() {
-    return this._isPlaying
-  }
-
-  executeTimer(play) {
-    if (play) this._playTimer()
-    else this._pauseTimer()
-  }
-
-  selectTimer(timer) {
-    this._updateInitialTime(timer)
-    this._updateCurrentTime()
-  }
-
-  reloadTimer() {
-    this._pauseTimer()
-    this._resetHours()
-    this._resetMinutes()
-    this._resetSeconds()
-  }
-
-  decreaseTimer() {
-    this._currentSeconds = this._currentSeconds > 0 ? this._currentSeconds - 1 : 59
-    this._currentMinutes = this._currentSeconds === 59 && this._currentMinutes > 0 ? this._currentMinutes - 1 : this._currentMinutes
-    this._currentHours = this._currentMinutes === 59 && this._currentHours > 0 ? this._currentHours - 1 : this._currentHours
-  }
-
-  timeIsOver() {
-    return this._currentHours === 0 && this._currentMinutes === 0 && this._currentSeconds === 0
-  }
-
-  getTimeFormatted() {
-    return this._formatHours() + ':' + this._formatMinutes() + ':' + this._formatSeconds()
-  }
-
-  _updateInitialTime(timer) {
-    const timerElements = timer.split(':')
-
-    this._initialHours = timerElements[0]
-    this._initialMinutes = timerElements[1]
-    this._initialSeconds = timerElements[2]
-  }
-
-  _updateCurrentTime() {
-    this._currentHours = parseInt(this._initialHours)
-    this._currentMinutes = parseInt(this._initialMinutes)
-    this._currentSeconds = parseInt(this._initialSeconds)
-  }
-
-  _playTimer() {
-    this._isPlaying = true
-  }
-
-  _pauseTimer() {
-    this._isPlaying = false
-  }
-
-  _resetHours() {
-    this._currentHours = parseInt(this._initialHours)
-  }
-
-  _resetMinutes() {
-    this._currentMinutes = parseInt(this._initialMinutes)
-  }
-
-  _resetSeconds() {
-    this._currentSeconds = parseInt(this._initialSeconds)
-  }
-
-  _formatHours() {
-    return this._currentHours >= 10 ? this._currentHours.toString() : '0' + this._currentHours.toString()
-  }
-
-  _formatMinutes() {
-    return this._currentMinutes >= 10 ? this._currentMinutes.toString() : '0' + this._currentMinutes.toString()
-  }
-
-  _formatSeconds() {
-    return this._currentSeconds >= 10 ? this._currentSeconds.toString() : '0' + this._currentSeconds.toString()
+    this.initialHours = '00'
+    this.initialMinutes = '05'
+    this.initialSeconds = '00'
+    this.currentHours = parseInt(this.initialHours)
+    this.currentMinutes = parseInt(this.initialMinutes)
+    this.currentSeconds = parseInt(this.initialSeconds)
+    this.isPlaying = false
   }
 }
---
+++ js/timerController.js
@@ -0,0 +1,88 @@
+import Timer from './timer.js'
+
+export default class TimerController {
+    _timer
+
+    constructor() {
+        this._timer = new Timer()
+    }
+
+    get isPlaying() {
+        return this._timer.isPlaying
+    }
+
+    executeTimer(play) {
+        if (play) this._playTimer()
+        else this._pauseTimer()
+    }
+
+    selectTimer(hours, minutes, seconds) {
+        this._updateInitialTime(hours, minutes, seconds)
+        this._updateCurrentTime()
+    }
+
+    reloadTimer() {
+        this._pauseTimer()
+        this._resetHours()
+        this._resetMinutes()
+        this._resetSeconds()
+    }
+
+    decreaseTimer() {
+        this._timer.currentSeconds = this._timer.currentSeconds > 0 ? this._timer.currentSeconds - 1 : 59
+        this._timer.currentMinutes = this._timer.currentSeconds === 59 && this._timer.currentMinutes > 0 ? this._timer.currentMinutes - 1 : this._timer.currentMinutes
+        this._timer.currentHours = this._timer.currentMinutes === 59 && this._timer.currentHours > 0 ? this._timer.currentHours - 1 : this._timer.currentHours
+    }
+
+    timeIsOver() {
+        return this._timer.currentHours === 0 && this._timer.currentMinutes === 0 && this._timer.currentSeconds === 0
+    }
+
+    getTimeFormatted() {
+        return this._formatHours() + ':' + this._formatMinutes() + ':' + this._formatSeconds()
+    }
+
+    _updateInitialTime(hours, minutes, seconds) {
+        this._timer.initialHours = hours
+        this._timer.initialMinutes = minutes
+        this._timer.initialSeconds = seconds
+    }
+
+    _updateCurrentTime() {
+        this._timer.currentHours = parseInt(this._timer.initialHours)
+        this._timer.currentMinutes = parseInt(this._timer.initialMinutes)
+        this._timer.currentSeconds = parseInt(this._timer.initialSeconds)
+    }
+
+    _playTimer() {
+        this._timer.isPlaying = true
+    }
+
+    _pauseTimer() {
+        this._timer.isPlaying = false
+    }
+
+    _resetHours() {
+        this._timer.currentHours = parseInt(this._timer.initialHours)
+    }
+
+    _resetMinutes() {
+        this._timer.currentMinutes = parseInt(this._timer.initialMinutes)
+    }
+
+    _resetSeconds() {
+        this._timer.currentSeconds = parseInt(this._timer.initialSeconds)
+    }
+
+    _formatHours() {
+        return this._timer.currentHours >= 10 ? this._timer.currentHours.toString() : '0' + this._timer.currentHours.toString()
+    }
+
+    _formatMinutes() {
+        return this._timer.currentMinutes >= 10 ? this._timer.currentMinutes.toString() : '0' + this._timer.currentMinutes.toString()
+    }
+
+    _formatSeconds() {
+        return this._timer.currentSeconds >= 10 ? this._timer.currentSeconds.toString() : '0' + this._timer.currentSeconds.toString()
+    }
+}
