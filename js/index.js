aa1374e6e71fc248c3e89f665b952ba4cb9229b2
nasciontem <lucas.tahansab@yahoo.com>
2021-07-06 14:33:24 -0300

    Trying to make the timer more precise

--- js/index.js
+++ js/index.js
@@ -6,44 +6,33 @@ const mobile = 'js/mobile.js'
 
 const timerController = new TimerController()
 const oneSecond = 1000
-let timerFormatted
+let timeFormatted
+let expected
+let timeout
 
 window.addEventListener('load', () => {
-  startTimer()
+  selectTimer('00', '05', '00')
   changeTimerValueOnScreen()
-  document.getElementById('5minutes').addEventListener('click', handlePresetTimeSelection)
-  document.getElementById('25minutes').addEventListener('click', handlePresetTimeSelection)
-  document.getElementById('50minutes').addEventListener('click', handlePresetTimeSelection)
   document.getElementById('execute').addEventListener('click', toggleTimerMode)
   document.getElementById('reload').addEventListener('click', reloadTimer)
-  document.getElementById('submit-message').addEventListener('click', submitMessage)
   document.getElementById('hours').addEventListener('keydown', () => { if (timerController.isPlaying) toggleTimerMode() })
   document.getElementById('minutes').addEventListener('keydown', () => { if (timerController.isPlaying) toggleTimerMode() })
   document.getElementById('seconds').addEventListener('keydown', () => { if (timerController.isPlaying) toggleTimerMode() })
+  document.getElementById('5minutes').addEventListener('click', handlePresetTimeSelection)
+  document.getElementById('25minutes').addEventListener('click', handlePresetTimeSelection)
+  document.getElementById('50minutes').addEventListener('click', handlePresetTimeSelection)
   document.getElementById('alarm').addEventListener('pause', reloadTimer)
+  document.getElementById('submit-message').addEventListener('click', submitMessage)
   if (mobileEnvironments.test(navigator.userAgent)) setUpEnvironmentWithModule(mobile)
   else setUpEnvironmentWithModule(desktop)
 })
 
 function setUpEnvironmentWithModule(module) {
-  const head = document.querySelector('head');
-  const script = document.createElement('script');
-  script.type = 'module';
-  script.src = module;
+  const head = document.querySelector('head')
+  const script = document.createElement('script')
+  script.type = 'module'
+  script.src = module
   head.appendChild(script)
-}
-
-function startTimer() {
-  setInterval(() => {
-    if (timerController.isPlaying) {
-      timerController.decreaseTimer()
-      changeTimerValueOnScreen()
-      if (timerController.timeIsOver()) {
-        timerController.reloadTimer()
-        playAlarm()
-      }
-    }
-  }, oneSecond)
 }
 
 function toggleTimerMode() {
@@ -56,6 +45,7 @@ function toggleTimerMode() {
 function toggleTimerModeWhenPlaying() {
   timerController.toggleTimerMode(false)
   changeExecuteImage()
+  stopTimer()
 }
 
 function toggleTimerModeWhenAlarmPlaying() {
@@ -67,53 +57,79 @@ function toggleTimerModeWhenAlarmPlaying() {
 function toggleTimerModeWhenNotPlaying() {
   timerController.toggleTimerMode(true)
   changeExecuteImage()
+  startTimer()
+}
+
+function handlePresetTimeSelection(event) {
+  const timeSplitted = event.target.dataset.time.split(':')
+  selectTimer(timeSplitted[0], timeSplitted[1], timeSplitted[2])
+  changeTimerValueOnScreen()
+  reloadTimer()
+}
+
+function stopTimer() {
+  clearTimeout(timeout)
+}
+
+function startTimer() {
+  expected = Date.now() + oneSecond
+  timeout = setTimeout(countDown, oneSecond)
+}
+
+function countDown() {
+  const drift = Date.now() - expected
+  expected += oneSecond
+  decreaseTimer()
+
+  if (!timerController.timeIsOver()) {
+    const newTimeout = drift < oneSecond && drift > 0 ? oneSecond - drift : oneSecond
+    timeout = setTimeout(countDown, newTimeout)
+  } else {
+    reloadTimer()
+    playAlarm()
+  }
 }
 
 function reloadTimer() {
+  stopTimer()
   timerController.reloadTimer()
   changeTimerValueOnScreen()
   changeExecuteImage()
   stopAlarm()
 }
 
-function handlePresetTimeSelection(event) {
-  const timerSplitted = event.target.dataset.time.split(':')
-  selectTimer(timerSplitted[0], timerSplitted[1], timerSplitted[2])
+function decreaseTimer() {
+  timerController.decreaseTimer()
   changeTimerValueOnScreen()
-  reloadTimer()
+}
+
+function selectTimer(hours, minutes, seconds) {
+  if (hours > 23) hours = 23
+  if (minutes > 59) minutes = 59
+  if (seconds > 59) seconds = 59
+  timerController.selectTimer(hours, minutes, seconds)
+  timeFormatted = timerController.getTimeFormatted()
+  updatePresetTimes()
+}
+
+function changeTimerValueOnScreen() {
+  timeFormatted = timerController.getTimeFormatted()
+  const timeSplitted = timeFormatted.split(':')
+  document.getElementById('hours').value = timeSplitted[0]
+  document.getElementById('minutes').value = timeSplitted[1]
+  document.getElementById('seconds').value = timeSplitted[2]
 }
 
 function updatePresetTimes() {
   const optionItems = document.querySelectorAll('.timer__options--item')
   optionItems.forEach(option => {
-    if (option.dataset.time === timerFormatted) option.classList.add('selected')
+    if (option.dataset.time === timeFormatted) option.classList.add('selected')
     else option.classList.remove('selected')
   })
 }
 
-function selectTimer(hours, minutes, seconds) {
-  hours = hours > 23 ? 23 : hours
-  minutes = minutes > 59 ? 59 : minutes
-  seconds = seconds > 59 ? 59 : seconds
-  timerController.selectTimer(hours, minutes, seconds)
-  timerFormatted = timerController.getTimeFormatted()
-  updatePresetTimes()
-}
-
-function changeTimerValueOnScreen() {
-  timerFormatted = timerController.getTimeFormatted()
-  const timerSplitted = timerFormatted.split(':')
-  document.getElementById('hours').value = timerSplitted[0]
-  document.getElementById('minutes').value = timerSplitted[1]
-  document.getElementById('seconds').value = timerSplitted[2]
-}
-
-function changeExecuteImage() {
-  document.getElementById('execute').src = timerController.isPlaying ? 'src/pause.png' : 'src/play.png'
-}
-
 function alarmIsPlaying() {
-  return document.getElementById('alarm').currentTime !== 0
+  return !document.getElementById('alarm').paused
 }
 
 function playAlarm() {
@@ -126,6 +142,10 @@ function stopAlarm() {
   const alarm = document.getElementById('alarm')
   alarm.pause()
   alarm.currentTime = 0
+}
+
+function changeExecuteImage() {
+  document.getElementById('execute').src = timerController.isPlaying ? 'src/pause.png' : 'src/play.png'
 }
 
 function submitMessage(event) {
