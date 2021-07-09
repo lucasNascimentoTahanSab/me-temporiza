028692b6b150c41d9cf1d213b83738cd4e37a16a
nasciontem <lucas.tahansab@yahoo.com>
2021-07-09 14:23:02 -0300

    Created email and emailController

---
+++ js/email.js
@@ -0,0 +1,15 @@
+export default class Email {
+  name
+  origin
+  title
+  message
+  destiny
+
+  constructor() {
+    this.name = ''
+    this.origin = ''
+    this.title = ''
+    this.message = ''
+    this.destiny = 'atendimento@metemporiza.com.br'
+  }
+}
---
+++ js/emailController.js
@@ -0,0 +1,9 @@
+import Email from "./email";
+
+export default class EmailController {
+  _email
+
+  constructor() {
+    this._email = new Email
+  }
+}
